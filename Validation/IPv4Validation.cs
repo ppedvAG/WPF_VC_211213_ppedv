1bbb6e425766c6838da0c7f631ed6e4c99e902aa
KlaasHagge <klaash@ppedv.de>
2021-12-15 14:19:28 +0100

    Trigger (kommentiert)

--- Validation/IPv4Validation.cs
+++ Validation/IPv4Validation.cs
@@ -7,11 +7,11 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace Validation
 {
-    public class ValidationRuleCollection : Collection<ValidationRule> { }
-
     public class IPv4Validation : ValidationRule
     {
         //ValidationRules müssen von der Klasse ValidationRule erben und die abstrakte Methode Validate() implementieren.
@@ -27,4 +27,31 @@ namespace Validation
                 return new ValidationResult(false, "Bitte gib eine IPv4-Adresse ein.");
         }
     }
+
+    //Eigene MarkupExtension zur Übergabe von als Ressourcen definierte ValidationRules (vgl. MainWindow.xaml unten)
+    public class ValidationMarkup : MarkupExtension
+    {
+        private Binding binding;
+
+        //Konstruktor (Die Übergabeparameter sind die Werte, welche das Objekt in XAML erwartet)
+        public ValidationMarkup(Binding binding, ValidationRuleCollection validationRules)
+        {
+            this.binding = binding;
+
+            //Hinzufügen der übergebenen Regeln an die Bindung
+            foreach (ValidationRule rule in validationRules)
+            {
+                binding.ValidationRules.Add(rule);
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            //Rückgabe der Bindung an XAML (bei Verwendung dieser MarkupExtension als Propertybelegung in XAML)
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+
+    //Collectionklasse zur Sammlung von ValidationRules (vgl. Window.Resources in MainWindow.xaml)
+    public class ValidationRuleCollection : Collection<ValidationRule> { }
 }
