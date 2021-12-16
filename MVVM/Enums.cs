f6643fce4eb93bfe88593089bb38fd4bd9003e37
KlaasHagge <klaash@ppedv.de>
2021-12-16 12:34:07 +0100

    MVVM

---
+++ MVVM/Enums.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    //Seperate Datei zur Definition von Enums, da diese sowohl im Model als auch im View verwendet werden
+    public enum Gender { Männlich, Weiblich, Divers }
+}
