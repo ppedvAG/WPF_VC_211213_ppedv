f49e0ef41deee0a331b97908e2e229d4e6240665
KlaasHagge <klaash@ppedv.de>
2021-12-16 16:45:54 +0100

    final (kommentiert)

---
+++ Adorner/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdornerBsp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Tbx_Show.Loaded += (s, e) =>
+            {
+                //Diese Methode durchläuft den VisualTree ausgehend von dem eingebenen UIElement nach oben und gibt das
+                //erste AdornerLayer zurück, welches es findet. Die Elemente müssen dafür schon vorhanden sein. Hier
+                //bekommen wir das Layer von der Textbox zurück.
+                AdornerLayer adLayer = AdornerLayer.GetAdornerLayer(Tbx_Show);
+
+                //Hinzufgen des Adorners zum Layer
+                adLayer.Add(new SimpleCircleAdorner(Tbx_Show));
+
+            };
+        }
+    }
+}
---
+++ Adorner/SimpleCircleAdorner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace AdornerBsp
+{
+    // Adorner-Klassen müssen von der abstrakten Klasse Adorner erben
+    public class SimpleCircleAdorner : Adorner
+    {
+        // Die Basisklasse muss durch den Konstruktor aufgerufen werden
+        public SimpleCircleAdorner(UIElement adornedElement)    //<= Das adornedElement ist das UIElement, welches ergänzt werden soll
+          : base(adornedElement)
+        {
+        }
+
+        // Standartmäßig wird das Renderverhalten des Adorners über die überschriebene OnRender-Methode implementiert.
+        // Diese wird vom Layout als Teil des Renderings des Windows etc aufgerufen.
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            //Erstellen des (unsichtbaren) umgebenden Rechtecks mittels der Größe des adornedElements
+            Rect adornedElementRect = new Rect(this.AdornedElement.DesiredSize);
+
+            //Voreinstellungen für den Pinsel der zu zeichnenden Kreise
+            SolidColorBrush renderBrush = new SolidColorBrush(Colors.Green);
+            renderBrush.Opacity = 0.2;
+            Pen renderPen = new Pen(new SolidColorBrush(Colors.Navy), 1.5);
+            double renderRadius = 5.0;
+
+            // Zeichnen der Kreise in die Ecken des Rechtecks
+            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.TopLeft, renderRadius, renderRadius);
+            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.TopRight, renderRadius, renderRadius);
+            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.BottomLeft, renderRadius, renderRadius);
+            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.BottomRight, renderRadius, renderRadius);
+        }
+    }
+}
